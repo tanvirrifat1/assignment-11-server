511ea6c7a0307e5969e903d6ed1077c8014b209e
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-11-08 02:01:08 +0600

    added momgodb

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleWares
 app.use(cors())
 app.use(express.json());
+
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASSWORD)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.afkplob.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
 
 app.get('/', (req, res) => {
     res.send('food service server is running')
