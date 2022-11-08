0bd2c02d60d3d1fd7ea16b776857cd0950b57df1
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-11-08 14:13:17 +0600

    added viewdetails

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,7 +32,12 @@ async function run() {
             res.send(getfood);
         });
 
-
+        app.get('/getfood/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await foodCollection.findOne(query)
+            res.send(service)
+        })
     }
     finally {
 
