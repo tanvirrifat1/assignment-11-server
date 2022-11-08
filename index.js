ec0c6b1719d5d3d7c80b750feae28bfb7ce0a3ea
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-11-08 13:40:21 +0600

    added getfood

--- index.js
+++ index.js
@@ -9,20 +9,36 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
-// console.log(process.env.DB_USER)
-// console.log(process.env.DB_PASSWORD)
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.afkplob.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
+        const foodCollection = client.db('foodService').collection('foods')
+
+        app.get('/foods', async (req, res) => {
+            const query = {};
+            const cursor = foodCollection.find(query);
+            const foods = await cursor.limit(3).toArray();
+            res.send(foods);
+        });
+
+        app.get('/getfood', async (req, res) => {
+            const query = {};
+            const cursor = foodCollection.find(query);
+            const getfood = await cursor.toArray();
+            res.send(getfood);
+        });
+
 
     }
     finally {
 
     }
 }
+run().catch(err => console.error(err))
 
 app.get('/', (req, res) => {
     res.send('food service server is running')
